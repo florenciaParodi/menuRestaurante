82a9c85d4d52a29e5fac6cf4b183fa8e06966a65
florenciaParodi <florenciaparodi1798@gmail.com>
2024-10-10 22:43:31 -0300

    agrego funciones de buscador

--- main.js
+++ main.js
@@ -4,55 +4,64 @@ console.log(bienvenida)
 let pedido = []
 console.log(pedido) */
 
-const hamburguesaBacon = { nombre: 'Hamburguesa Bacon', pan: 'Pan de Papa', medallon: 'Roast Beef', gr: 200, lechuga: true, tomate: true, cheddar: true, bacon: true, aderezo: true, valor: 9000 }
-const hamburguesaCheddar = { nombre: 'Hamburguesa Cheddar', pan: 'Pan de Chipa', medallon: 'Roast Beef', gr: 200, lechuga: true, tomate: true, cheddar: true, bacon: false, aderezo: false, valor: 9000 }
-const hamburguesaPollo = { nombre: 'Hamburguesa de Pollo', pan: 'Pan de Papa', medallon: 'Pollo Crispy', gr: 200, lechuga: true, tomate: true, cheddar: true, bacon: false, aderezo: false, valor: 9000 }
-const hamburguesaVegana = { nombre: 'Hamburguesa Vegana', pan: 'Pan de Papa Vegano', medallon: 'Lentejas y Berenjena', gr: 200, lechuga: true, tomate: true, cheddar: false, bacon: false, aderezo: true, valor: 9000 }
-const hamburguesaSinTacc = { nombre: 'Hamburguesa Sin TACC', pan: 'Pan de Harina de Arroz ', medallon: 'Roast Beef', gr: 200, lechuga: true, tomate: true, cheddar: false, bacon: false, aderezo: false, valor: 9000 }
+const hamburguesaBacon = { nombre: 'Hamburguesa Bacon', valor: 9000 }
+const hamburguesaCheddar = { nombre: 'Hamburguesa Cheddar', valor: 9000 }
+const hamburguesaPollo = { nombre: 'Hamburguesa de Pollo', valor: 9000 }
+const hamburguesaVegana = { nombre: 'Hamburguesa Vegana',  valor: 9000 }
+const hamburguesaSinTacc = { nombre: 'Hamburguesa Sin TACC', valor: 9000 }
 
-const cocaCola = { nombre: 'Coca Cola Regular', ml: 500, light: false, valor: 1800, }
-const cocaColaLight = { nombre: 'Coca Cola Light', ml: 500, light: true, valor: 1800, }
-const fanta = { nombre: 'Fanta', ml: 500, light: false, valor: 1800, }
-const spriteZero = { nombre: 'Sprite Zero', ml: 500, light: true, valor: 1800, }
-const aguaSinGas = { nombre: 'Agua Sin Gas', ml: 500, light: true, valor: 2000, }
-const aguaConGas = { nombre: 'Agua Con Gas', ml: 500, light: true, valor: 2000, }
+const cocaCola = { nombre: 'Coca Cola Regular',valor: 1800, }
+const cocaColaLight = { nombre: 'Coca Cola Light',valor: 1800, }
+const fanta = { nombre: 'Fanta', valor: 1800, }
+const spriteZero = { nombre: 'Sprite Zero', valor: 1800, }
+const aguaSinGas = { nombre: 'Agua Sin Gas',valor: 2000, }
+const aguaConGas = { nombre: 'Agua Con Gas', valor: 2000, }
 
-const papasFritas = { nombre: 'Papas Fritas Clásicas', cheddar: false, aderezo: false, valor: 3500 }
-const papasCheddar = { nombre: 'Papas Fritas con Cheddar', cheddar: true, aderezo: false, valor: 3500 }
-const arosCebolla = { nombre: 'Aros de Cebolla', cheddar: false, aderezo: true, valor: 3500 }
-const batatasFritas = { nombre: 'Batatas Fritas  ', cheddar: false, aderezo: false, valor: 3500 }
+const papasFritas = { nombre: 'Papas Fritas Clásicas', valor: 3500 }
+const papasCheddar = { nombre: 'Papas Fritas con Cheddar', valor: 3500 }
+const arosCebolla = { nombre: 'Aros de Cebolla', valor: 3500 }
+const batatasFritas = { nombre: 'Batatas Fritas  ', valor: 3500 }
 
-const hamburguesas = [hamburguesaBacon, hamburguesaCheddar, hamburguesaPollo, hamburguesaVegana, hamburguesaSinTacc]
-const bebidas = [cocaCola, cocaColaLight, fanta, spriteZero, aguaSinGas, aguaConGas]
-const acompaniamientos = [papasFritas, papasCheddar, arosCebolla, batatasFritas]
+const productos = [hamburguesaBacon, hamburguesaCheddar, hamburguesaPollo, hamburguesaVegana, hamburguesaSinTacc, cocaCola, cocaColaLight, fanta, spriteZero, aguaSinGas, aguaConGas, papasFritas, papasCheddar, arosCebolla, batatasFritas]
+
+let itemsAmostrar = [...productos]
+
+let buscadorInput = document.createElement('input')
+let buscadorLabel = document.createElement('label')
+buscadorLabel.innerText = 'Buscar...'
+buscadorLabel.setAttribute('for', 'searchInput')
+buscadorInput.setAttribute('type', 'search')
+buscadorInput.setAttribute('id', 'searchInput')
+buscadorInput.addEventListener('change', event => {
+    let text = event.target.value
+    if (text.trim() !== ''){
+        itemsAmostrar = productos.filter(producto => producto.nombre.toLowerCase().includes(text.toLowerCase().trim()))
+    } else {
+        itemsAmostrar = [...productos]
+    }
+    showMenu();
+})
+let buscador = document.getElementById('searchBar')
+buscador.appendChild(buscadorLabel)
+buscador.appendChild(buscadorInput)
+
 
 let productSection = document.getElementById('productos')
-hamburguesas.forEach((hamburguesa) => {
-    let productCard = document.createElement('div')
-    productCard.className = 'card'
-    productCard.innerHTML = `<b>${hamburguesa.nombre}</b><p> Detalles: ${hamburguesa.pan} -  Medallón : ${hamburguesa.medallon}, ${hamburguesa.gr}gr <p>$ ${hamburguesa.valor}</p><button id=restarUno>-</button><span id=contador>0</span><button id=sumarUno>+</button><button>Agregar al pedido</button>`
-    productSection.appendChild(productCard)
-})
-bebidas.forEach((bebida) => {
-    let productCard = document.createElement('div')
-    productCard.className = 'card'
-    productCard.innerHTML = `<b>${bebida.nombre}</b><p> ${bebida.ml}ml <p>$ ${bebida.valor}</p> <button>Agregar al pedido</button> `
-    productSection.appendChild(productCard)
-})
-acompaniamientos.forEach((acompaniamiento) => {
-    let productCard = document.createElement('div')
-    productCard.className = 'card'
-    productCard.innerHTML = `<b>${acompaniamiento.nombre}</b> <p>$ ${acompaniamiento.valor}</p> <button>Agregar al pedido</button>`
-    productSection.appendChild(productCard)
-})
 
-let buscador = document.getElementById('searchBar')
-buscador.innerHTML = '<label for="buscar">Buscar ...</label> <input type="search">'
-buscador.onkeyup = () => {
-    
+const showMenu = () => {
+    productSection.innerHTML = ''
+    itemsAmostrar.forEach((producto) => {
+        let productCard = document.createElement('div')
+        productCard.className = 'card'
+        productCard.innerHTML = `<b>${producto.nombre}</b><p>$ ${producto.valor}</p><button id=restarUno>-</button><span id=contador>0</span><button id=sumarUno>+</button><button>Agregar al pedido</button>`
+        productSection.appendChild(productCard)
+    })
 }
+showMenu();
 
-let restarUno = document.getElementById('restarUno')
+let carrito = docuement.getElementById('cart')
+
+/*let restarUno = document.getElementById('restarUno')
 let sumarUno = document.getElementById('sumaruno')
 let contador = document.genElementById('contador')
 let cantidad = 0
@@ -64,9 +73,9 @@ sumarUno.onclick = () => {
 restarUno.onclick = () => {
     cantidad--
     contador.innerHTML = cantidad
-}
+} */
 
-//carrito : tengo que crear unn elemento como un input que sea un contador y vaya agregando los productod pero que también permita ver una lista del pedido 
+//carrito : tengo que crear unn elemento como un input que sea un contador y vaya agregando los productos pero que también permita ver una lista del pedido 
 // botón de remove producto : botón eliminiar producto en cada producto del carrito y que sea .onclick, remove()
 // on key up : cuando el usuario tipee una letra, ir mostrando resultados en el buscador en formato de lista
 // contador si quiere poner el + y el - en cada producto : let contador = 0 y después, .onclick , click++ y mostrarlo en el input vacio
